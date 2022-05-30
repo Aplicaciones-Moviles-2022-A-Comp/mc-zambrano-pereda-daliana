d73852858e821c067ae130cf2719cc43f844233b
Daliana Zambrano <daliana.zambrano@epn.edu.ec>
2022-05-30 12:06:02 -0500

    Main1_30052022

--- 00-Kotlin/Proyecto-Kotlin/src/main/kotlin/Main.kt
+++ 00-Kotlin/Proyecto-Kotlin/src/main/kotlin/Main.kt
@@ -61,7 +61,7 @@ fun main () {
     println(Suma.historialSumas)
 
     //ARREGLOS - Tipos
-    
+
     // Arreglo Estatico
     val arregloEstatico: Array<Int> = arrayOf(1, 2, 3)
     println(arregloEstatico)
@@ -73,7 +73,68 @@ fun main () {
     arregloDinamico.add(12)
     println(arregloDinamico)
 
+    // OPERADORES -> Sirven para los arreglos estáticos y dinámicos
 
+
+    // FOR EACH -> Unit
+    // Iterar un arreglo
+
+    val respuestaForEach: Unit = arregloDinamico
+        .forEach {
+                valorActual: Int ->
+            println("Valor actual: ${valorActual}")
+        }
+    arregloDinamico
+        .forEachIndexed { indice: Int, valorActual: Int ->
+            println("Valor ${valorActual} Indice: ${indice}")
+        }
+    println(respuestaForEach)
+
+    // MAP -> Muta el arreglo (Cambia el arreglo)
+    // 1) Enviemos el nuevo valor de la iteracion
+    // 2) Nos devuelve es un NUEVO ARREGLO con los valores modificados
+
+    val respuestaMap: List<Double> = arregloDinamico
+        .map { valorActual: Int ->
+            return@map valorActual.toDouble() + 100.00
+        }
+
+    println(respuestaMap)
+
+    val respuestaMapDos = arregloDinamico.map { it + 15 }
+//        .map { valorActual: Int ->
+//            return@map valorActual + 15
+//        }
+
+    println(respuestaMapDos)
+
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresion (TRUE o FALSE)
+    // 2) Nuevo arreglo filtrado
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual: Int ->
+            val mayoresACinco: Boolean = valorActual > 5// Expresion Condicion
+            return@filter mayoresACinco
+        }
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    //OR AND
+    //OR -> ANY (Alguno cumple?)
+    //AND -> ALL (Todos cumplen?)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any {valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) //true
+
+    val respuestaAll: Boolean = arregloDinamico
+        .all {valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll) //false
 }
 
 //función para imprimir
